20a02acbd7632361b3908f8389698d621f22aae2
Javier Azanza <azzanza@gmail.com>
2022-05-09 22:17:06 -0500

    Ejercicios del dia 1 completos

---
+++ dia1.py
@@ -0,0 +1,18 @@
+print("Me encanta estudiar python")
+
+print("Estudiar con 'Python total' es super divertido")
+
+print(321+234)
+
+print(f'Linea 1\nLinea 2\nLinea 3')
+
+print('a\tb\tc\nd\te\tf\ng\th\ti')
+
+print("Diagonal: / \nDiagonal invertida: \\")
+
+print(input("Que estas estudiando: "))
+
+print(input("En que pais Vives: "))
+
+print(input("cual es tu nombre: "),
+      input("cual es tu apellido"))
---
+++ proyecto1.py
@@ -0,0 +1,4 @@
+nombre = input("Cual es tu nombre: ")
+tipo = input("De que tipo es tu cerveza: ")
+
+print(f'Bienvenidos a la cerveceria {tipo} de "{nombre}"')
