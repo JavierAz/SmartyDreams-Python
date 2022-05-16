88dd5fec8c882b36146aece43f97821c8b531831
Javier Azanza <azzanza@gmail.com>
2022-05-15 20:41:09 -0500

    proyecto del dia 4 completo

--- dia4/proyecto4.py
+++ dia4/proyecto4.py
@@ -4,3 +4,27 @@
 # validaciones en rangos
 # si es que leatina
 
+from random import randint
+
+vidas = 10
+intentos = 0
+numero = randint(1, 100)
+nombre = input('Ingresa tu nombre: ')
+
+print(
+    f'{nombre} Tienes que  adivinar un numero que pense entre 1 y 100, tienes {vidas} vidas.\nQue la suerte este contigo!')
+
+while vidas >= 0:
+    intentos = int(input('Prueba: '))
+    vidas += 1
+
+    if intentos < numero:
+        print('Un numero mas grande')
+    elif intentos > numero:
+        print('Un numero mas chico')
+    else:
+        print(f'{nombre.capitalize()} en Horabuena felicidades!')
+        break
+
+if intentos != numero:
+    print(f'Haz perdido, el numero era: {numero}')
